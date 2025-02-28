3d205d046c1242d391bda8dec63b91e6b9eed34f
aigerim0609 <aigerim062009rasulovna@gmail.com>
2025-02-28 14:55:35 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+const gameContainer = document.querySelector(".container"),
+  userResult = document.querySelector(".user_result img"),
+  cpuResult = document.querySelector(".cpu_result img"),
+  result = document.querySelector(".result"),
+  optionImages = document.querySelectorAll(".option_image");
+
+optionImages.forEach((image, index) => {
+  image.addEventListener("click", (e) => {
+    image.classList.add("active");
+
+    optionImages.forEach((image2, index2) => {
+      index !== index2 && image2.classList.remove("active");
+   });
+   
+    let imageSrc = e.target.querySelector("img").src;
+    userResult.src = imageSrc;
+
+    let randomNumber = Math.floor(Math.random() * 3);
+
+    let cpuImages = ["./rock.png", "./paper.png", "./scissor.png"];
+    cpuResult.src = cpuImages[randomNumber];
+
+    let cpuValue = ["R", "P", "S"][randomNumber];
+    let userValue = ["R", "P", "S"][index];
+
+    console.log(cpuValue, userValue);
+
+    let outcomes = {
+      RR: "Draw",
+      RP: "Cpu",
+      RS: "User",
+      PP: "Draw",
+      PR: "User",
+      PS: "Cpu",
+      SS: "Draw",
+      SR: "Cpu",
+      SP: "User",
+    };
+
+    let outComeValue = outcomes[userValue + cpuValue];
+
+    result.textContent =
+      userValue === cpuValue ? "Match Draw" : `${outComeValue} Won!!`;
+    console.log(outComeValue);
+  });
+});
